a19d7dd546c449b94723285f83d4972db21d0da4
Wambura01 <brianwambura96@gmail.com>
2021-08-23 16:55:52 +0300

    Initialized a react native app and made sure it run on emulator

---
+++ constants/index.js
@@ -0,0 +1,6 @@
+import images from "./images";
+
+import { COLORS, FONTS, SIZES } from "./theme";
+import GOOGLE_API_KEY from "./maps";
+
+export { icons, images, COLORS, SIZES, FONTS, GOOGLE_API_KEY };

5522c361e76160dfa6ddd3dca73a9e8797501c56
Wambura01 <brianwambura96@gmail.com>
2021-08-26 22:51:07 +0300

    Added the header component on the home screen with icons and styles. The location text will change dynamically in the future build

--- constants/icons.js
+++ constants/icons.js
@@ -2,5 +2,7 @@ export const cutlery = require('../assets/icons/cutlery.png');
 export const search = require('../assets/icons/search.png');
 export const like = require('../assets/icons/like.png');
 export const user = require('../assets/icons/user.png');
+export const nearby = require('../assets/icons/nearby.png');
+export const basket = require('../assets/icons/basket.png');
 
-export default {cutlery, search, like, user};
+export default {cutlery, search, like, user, nearby, basket};
--- screens/Home.js
+++ screens/Home.js
@@ -1,12 +1,84 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  FlatList,
+} from 'react-native';
+
+import {images, SIZES, COLORS, FONTS} from '../constants/theme';
+import icons from '../constants/icons';
 
 const Home = () => {
-  return (
-    <View>
-      <Text>Home</Text>
-    </View>
-  );
+  const renderHeader = () => {
+    return (
+      <View style={{flexDirection: 'row', height: 50}}>
+        {/* left top icon */}
+        <TouchableOpacity
+          style={{
+            width: 50,
+            paddingLeft: SIZES.padding * 2,
+            justifyContent: 'center',
+          }}>
+          <Image
+            source={icons.nearby}
+            resizeMode="contain"
+            style={{width: 30, height: 30}}
+          />
+        </TouchableOpacity>
+        {/* middle location text */}
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+          <View
+            style={{
+              width: '70%',
+              height: '100%',
+              backgroundColor: COLORS.lightgray3,
+              alignItems: 'center',
+              justifyContent: 'center',
+              borderRadius: SIZES.radius,
+            }}>
+            <Text style={{...FONTS.h3}}>Location</Text>
+          </View>
+        </View>
+        {/* right icon */}
+        <TouchableOpacity
+          style={{
+            width: 50,
+            paddingRight: SIZES.padding * 2,
+            justifyContent: 'center',
+          }}>
+          <Image
+            source={icons.basket}
+            resizeMode="contain"
+            style={{width: 30, height: 30}}
+          />
+        </TouchableOpacity>
+      </View>
+    );
+  };
+
+  return <SafeAreaView style={styles.container}>{renderHeader()}</SafeAreaView>;
 };
+
+//styles
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.lightGray4,
+  },
+  shadow: {
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.1,
+    shadowRadius: 3,
+    elevation: 1,
+  },
+});
 
 export default Home;
